f5fe96b7db17e7a257d837eb6f02dc0cd5411b50
noanoapg <akgsgrpg@gmail.com>
2025-08-10 23:41:32 +0900

    保存

--- test.java
+++ test.java
@@ -1,3 +1,8 @@
 public class test {
-    
-}
+    public static void main(String[] args) {
+        int c = 5;
+        int a = 10;
+        System.out.println(a + c);
+        System.out.println("こんにちは、世界");
+    }
+}    
